5b69773d8ee694a519d5078210c3ff9752e8bc9c
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-01-29 17:46:13 -0800

    Generated 1999-2009 data again due to issues.  2011 is haulted until I find more data

--- src/clean2011.R
+++ src/clean2011.R
@@ -15,9 +15,12 @@ df2011.check <- read_excel("input/Yields and Residue HY2011 112311.xls",
   filter(!is.na(UID) | (!is.na(Row) & !is.na(Column))) %>% 
   arrange(UID)
 
-# Values in df2010 were copy pasted from df2010.check, but make sure at least one column matches
-check <- df2011$`Total Grain Wet (g)` == df2011.check$`Total Grain Wet (g)`
-if(length(check) < length(df2011$`Total Grain Wet (g)`)) {stop("Error")}
+# Values in df2010 were copy pasted from df2010.check, but make sure at least two columns match
+check.grain.wet <- df2011$`Total Grain Wet (g)` == df2011.check$`Total Grain Wet (g)`
+if(length(check.grain.wet) < length(df2011$`Total Grain Wet (g)`)) {stop("Error")}
+check.biomass.wet <- df2011$`Total Residue and Grain Wet (g)` == df2011.check$`Total Residue and Grain Wet (g)`
+if(length(check.biomass.wet) < length(df2011$`Total Residue and Grain Wet (g)`)) {stop("Error")}
+
 
 # Merge the georef data based on col and row2
 df <- append_georef_to_df(df2011, "Row", "Column")
@@ -27,13 +30,23 @@ df.id2.check <- df %>%
   filter(df$UID != df$ID2)
 if(nrow(df.id2.check) > 0) { stop("Error in ID2, Row2, Col") }
 
-# There are values where ID2, Row2, and Col do not mesh, after review, choose ID2, not UID
+# There are values where ID2, Row2, and Col do not mesh, after review, chose ID2, not UID
 
-# TODO: Incomplete, need dry weights, need to calc yield, need residue, need C, N values... ahhhh!!!
+# TODO: Incomplete, need dry weights, need to calc dry yield, need dry residue, need C, N values... ahhhh!!!
+df.calcs <- df %>% 
+  mutate(Comments = case_when(is.na(as.numeric(df$`Total Residue and Grain Wet (g)`)) ~ paste("Residue note: ", `Total Residue and Grain Wet (g)`, sep = ""))) %>% 
+  mutate(Comments = case_when(is.na(as.numeric(df$`Total Grain Wet (g)`)) ~ paste(Comments, " | Grain note: ", `Total Grain Wet (g)`, sep = ""))) %>% 
+  mutate(BiomassWet = as.numeric(`Total Residue and Grain Wet (g)`)) %>% 
+  mutate(GrainMassWet = as.numeric(`Total Grain Wet (g)`)) %>% 
+  mutate(ResidueMassWetPerArea = BiomassWet - GrainMassWet / `Area (m2)`) %>% 
+  mutate(GrainYieldWetPerArea = GrainMassWet / `Area (m2)`)
+
+# TODO: Figure out if "wet" here means dry, or if there are missing data somewhere...
 #df.clean <- df %>% 
 #  select(Year,
 #         `Current Crop`,
 #         Barcode,
 #         X,
 #         Y,
-#         ID2)
+#         ID2,
+#         )
