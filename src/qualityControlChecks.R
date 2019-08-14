dc0635482153cb72ad3bcc26e12e2a3fb7b276d6
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-08-14 08:40:47 -0700

    After review with Dr. Huggins, ID2 16 in 2014 found to have issue with residue, not yield

--- src/qualityControlChecks.R
+++ src/qualityControlChecks.R
@@ -10,10 +10,8 @@ remove_calculated_values_manually <- function(x) {
     mutate(ResidueMassDryPerArea = case_when(
       (HarvestYear == 2015 & ID2 == 169) ~ NA_real_,
       (HarvestYear == 2002 & ID2 == 252) ~ NA_real_,
-      TRUE ~ ResidueMassDryPerArea)) %>% 
-    mutate(GrainYieldDryPerArea = case_when(
-      (HarvestYear == 2014 & ID2 == 16) ~ NA_real_,
-      TRUE ~ GrainYieldDryPerArea))
+      (HarvestYear == 2014 & ID2 == 16) ~ NA_real_
+      TRUE ~ ResidueMassDryPerArea))
   
   return(x.rm.outliers)
 }
