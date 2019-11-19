6df62819b9ee85f038b60a2c692d8568ff6fdf6a
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-11-19 13:55:53 -0800

    Restructuring function calls resulted in a gap-filling function generating scrubbed data again -- fixed this

--- src/qualityControlChecks.R
+++ src/qualityControlChecks.R
@@ -7,13 +7,23 @@ remove_calculated_values_manually <- function(x) {
   #   x = dataframe with calculated values, as produced by get_clean1999_2016() or estimateResidueMassDryXByResidueMoistureProportion()
   
   require(tidyverse)
-  
+
   x.rm.outliers <- x %>% 
     mutate(ResidueMassDryPerArea = case_when(
       (HarvestYear == 2015 & ID2 == 169) ~ NA_real_,
       (HarvestYear == 2002 & ID2 == 252) ~ NA_real_,
       (HarvestYear == 2014 & ID2 == 16) ~ NA_real_,
-      TRUE ~ ResidueMassDryPerArea))
+      TRUE ~ ResidueMassDryPerArea)) %>% 
+    mutate(ResidueMassDry = case_when(
+      (HarvestYear == 2015 & ID2 == 169) ~ NA_real_,
+      (HarvestYear == 2002 & ID2 == 252) ~ NA_real_,
+      (HarvestYear == 2014 & ID2 == 16) ~ NA_real_,
+      TRUE ~ ResidueMassDry)) %>% 
+    mutate(ResidueMassWet = case_when(
+      (HarvestYear == 2015 & ID2 == 169) ~ NA_real_,
+      (HarvestYear == 2002 & ID2 == 252) ~ NA_real_,
+      (HarvestYear == 2014 & ID2 == 16) ~ NA_real_,
+      TRUE ~ ResidueMassWet))
   
   return(x.rm.outliers)
 }
